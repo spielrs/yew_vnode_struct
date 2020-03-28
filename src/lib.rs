63307f27b5c06e8c4831724b6650457da98aa65f
spieljs <spieljs@gmail.com>
2020-03-28 17:30:16 +0100

    improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,90 @@
+//! This module contains the implementation to get all the VNode structure.
+//! it is useful for testing and debuging. For example to know what contain this VNode
+//! we use VNodeStruct to get the complete Vnode button and log the result:
+//!
+//! #Example
+//!
+//! ```
+//! extern crate yew;
+//! extern crate yew_vnode_struct;
+//!
+//! use yew::html;
+//! use yew_vnode_struct::VNodeStruct;
+//!
+//! let example = html! {
+//!     <div id="example">{"example"}</div>
+//! };
+//!
+//! let vnode_example = VNodeStruct::new(example.clone());
+//!
+//! println!("{:#?}", example);
+//! ```
+//!
+//! We will get this structure:
+//!
+//! ```ignore
+//! VNodeStruct {
+//!     vtag: Some(
+//!         VTagStruct {
+//!             reference: None,
+//!             attributes: {
+//!                 "id": "example",
+//!             },
+//!             classes: Classes {
+//!                 set: {},
+//!             },
+//!             value: None,
+//!             kind: None,
+//!             checked: false,
+//!             node_ref: NodeRef(
+//!                 RefCell {
+//!                     value: NodeRefInner {
+//!                         node: None,
+//!                         link: None,
+//!                     },
+//!                 },
+//!             ),
+//!         },
+//!     ),
+//!     vlist: None,
+//!     vtext: None,
+//!     vcomp: None,
+//!     vref: None,
+//!     children: Some(
+//!         [
+//!             VNodeStruct {
+//!                 vtag: None,
+//!                 vlist: Some(
+//!                     VList {
+//!                         children: [
+//!                             VText { text: example },
+//!                         ],
+//!                         elide_placeholder: true,
+//!                     },
+//!                 ),
+//!                 vtext: None,
+//!                 vcomp: None,
+//!                 vref: None,
+//!                 children: Some(
+//!                     [
+//!                         VNodeStruct {
+//!                             vtag: None,
+//!                             vlist: None,
+//!                             vtext: Some(
+//!                                 VText { text: example },
+//!                             ),
+//!                             vcomp: None,
+//!                             vref: None,
+//!                             children: None,
+//!                         },
+//!                     ],
+//!                 ),
+//!             },
+//!         ],
+//!     ),
+//! }
+//! ```
+
 pub mod vnode_struct;
 
 pub use vnode_struct::VNodeStruct;
--- src/vnode_struct.rs
+++ src/vnode_struct.rs
@@ -1,84 +1,3 @@
-//! This module contains the implementation to get all the VNode structure.
-//! it is useful for testing and debuging. For example we want to know what contain this VNode:
-//! ```ignore
-//! let example = html! {
-//!     <div id="example">{"example"}</div>
-//! };
-//! ```
-//!
-//! Now we use VNodeStruct to get the complete Vnode button and log the result:
-//!
-//! ```ignore
-//!   let vnode_example = VNodeStruct::new(example);
-//!
-//!   console.log(&format!("{:#?}", example));
-//! ```
-//!
-//! We will get this in the console:
-//!
-//! ```ignore
-//! VNodeStruct {
-//!     vtag: Some(
-//!         VTagStruct {
-//!             reference: None,
-//!             attributes: {
-//!                 "id": "example",
-//!             },
-//!             classes: Classes {
-//!                 set: {},
-//!             },
-//!             value: None,
-//!             kind: None,
-//!             checked: false,
-//!             node_ref: NodeRef(
-//!                 RefCell {
-//!                     value: NodeRefInner {
-//!                         node: None,
-//!                         link: None,
-//!                     },
-//!                 },
-//!             ),
-//!         },
-//!     ),
-//!     vlist: None,
-//!     vtext: None,
-//!     vcomp: None,
-//!     vref: None,
-//!     children: Some(
-//!         [
-//!             VNodeStruct {
-//!                 vtag: None,
-//!                 vlist: Some(
-//!                     VList {
-//!                         children: [
-//!                             VText { text: example },
-//!                         ],
-//!                         elide_placeholder: true,
-//!                     },
-//!                 ),
-//!                 vtext: None,
-//!                 vcomp: None,
-//!                 vref: None,
-//!                 children: Some(
-//!                     [
-//!                         VNodeStruct {
-//!                             vtag: None,
-//!                             vlist: None,
-//!                             vtext: Some(
-//!                                 VText { text: example },
-//!                             ),
-//!                             vcomp: None,
-//!                             vref: None,
-//!                             children: None,
-//!                         },
-//!                     ],
-//!                 ),
-//!             },
-//!         ],
-//!     ),
-//! }
-//! ```
-
 use std::collections::HashMap;
 use web_sys::{Element, Node};
 use yew::html::NodeRef;
@@ -111,7 +30,7 @@ impl From<VTag> for VTagStruct {
     }
 }
 
-/// all the properties of VNodeStruct
+/// All the properties of VNodeStruct
 #[derive(Debug, PartialEq)]
 pub struct VNodeStruct {
     vtag: Option<VTagStruct>,
